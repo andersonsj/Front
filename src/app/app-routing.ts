1b83775f27542981b2f626e234fa400a78f116e5
AndersonJimenez <asjimenez@asesoftware.com>
2018-11-19 11:28:23 -0500

    Solucion de estructura

---
+++ src/app/app-routing.ts
@@ -0,0 +1,33 @@
+import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/layout/app-layouts/main-layout/main-layout.component';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        data: { pageTitle: 'Home' },
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'cliente',
+                loadChildren: 'app/component/cliente/cliente.module#ClienteModule'
+            },
+            {
+                path: 'empleado',
+                loadChildren: 'app/component/empledo/empleado.module#EmpleadoModule'
+            },
+            {
+                path: 'producto',
+                loadChildren: 'app/component/produto/producto.module#ProductoModule'
+            }
+        ]
+    },
+
+];
+
+export const Routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: false });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './component/home/home.component';
 import { UrlproviderService } from './util/endpoint/urlprovider.service';
 import { LayoutModule } from './shared/layout/layout.module';
 import { ClienteModule } from './component/cliente/cliente.module';
 import { ProductoModule } from './component/producto/producto.module';
 import { EmpleadoModule } from './component/empleado/empleado.module';
+import { Routing } from './app-routing';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import { EmpleadoModule } from './component/empleado/empleado.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    Routing,
     LayoutModule,
     ClienteModule,
     EmpleadoModule,
