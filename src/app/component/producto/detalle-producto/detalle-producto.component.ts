07b7beefa6514eb4054e6a33f6e405c455d00dc9
Oswaldo Galindo <osgapri@gmail.com>
2018-11-29 17:47:25 -0500

    Se carga información de detalle de productos

--- src/app/component/producto/detalle-producto/detalle-producto.component.ts
+++ src/app/component/producto/detalle-producto/detalle-producto.component.ts
@@ -8,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class DetalleProductoComponent implements OnInit {
 
   private cantidadProducto: any;
+  private skuProducto: any;
+  private nombre: any;
+  private precio: any;
+  private precioCmr: any;
+  private garantia: any;
+  private nroPuestos: any;
+  private relleno: any;
+  private materialPatas: any;
+  private modelo: any;
+  private observaciones: any;
   constructor() { }
 
   ngOnInit() {
     this.cantidadProducto = localStorage.getItem('cantidadProducto');
+    this.skuProducto = localStorage.getItem('skuProducto');
+    this.nombre = localStorage.getItem('nombre');
+    this.precio = localStorage.getItem('precio');
+    this.precioCmr = localStorage.getItem('precioCmr');
+    this.garantia = localStorage.getItem('garantia');
+    this.nroPuestos = localStorage.getItem('nroPuestos');
+    this.relleno = localStorage.getItem('relleno');
+    this.materialPatas = localStorage.getItem('materialPatas');
+    this.modelo = localStorage.getItem('modelo');
+    this.observaciones = localStorage.getItem('observaciones');
+    
+    console.log("sku producto detalle: "+ this.skuProducto);
     this.cantidadProducto = 1;
   }
 
--- src/app/component/producto/listar-producto/listar-producto.component.ts
+++ src/app/component/producto/listar-producto/listar-producto.component.ts
@@ -18,6 +18,16 @@ export class ListarProductoComponent implements OnInit {
   private datos: any;
   private cantidadProducto: any = 1;
   private sku: any;
+  private nombre: any;
+  private precio: any;
+  private precioCmr: any;
+  private garantia: any;
+  private nroPuestos: any;
+  private relleno: any;
+  private materialPatas: any;
+  private modelo: any;
+  private observaciones: any;
+  private ficha: any;
 
   constructor(private productoService: ProductoService, private formBuilder: FormBuilder) { }
 
@@ -39,12 +49,38 @@ export class ListarProductoComponent implements OnInit {
     );
   }
 
+  identificarProducto(identificador: any){
+    console.log("identificador: "+identificador);
+    this.sku = this.datos[identificador].sku;
+    this.nombre = this.datos[identificador].nombre;
+    this.precio = this.datos[identificador].precio;
+    this.precioCmr = this.datos[identificador].cmr;
+    this.garantia = this.datos[identificador].garantia;
+    this.nroPuestos = this.datos[identificador].ficha.nroPuestos;
+    this.relleno = this.datos[identificador].ficha.relleno;
+    this.materialPatas = this.datos[identificador].ficha.materialPatas;
+    this.modelo = this.datos[identificador].ficha.modelo;
+    this.observaciones = this.datos[identificador].ficha;
+    this.ficha = this.datos[identificador].ficha;
+    console.log("sku: " + this.sku);
+
+  }
+
   guardarDetalle(){
     console.log("Cantidad de producto");
     console.log(this.productoForm.get('txtCantidad').value);
     localStorage.setItem('cantidadProducto', this.productoForm.get('txtCantidad').value);
-    this.sku = this.datos.sku;
-    console.log("sku: " + this.sku);
+    localStorage.setItem('skuProducto', this.sku);
+    localStorage.setItem('nombre', this.nombre);
+    localStorage.setItem('precio', this.precio);
+    localStorage.setItem('precioCmr', this.precioCmr);
+    localStorage.setItem('garantia', this.garantia);
+    localStorage.setItem('nroPuestos', this.nroPuestos);
+    localStorage.setItem('relleno', this.relleno);
+    localStorage.setItem('materialPatas', this.materialPatas);
+    localStorage.setItem('modelo', this.modelo);
+    localStorage.setItem('observaciones', this.observaciones);
+    localStorage.setItem('ficha', JSON.stringify(this.ficha));
   }
 
   cantidadIni(){ 
