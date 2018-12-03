c9deaa9f8beadb307f3d810be65bb7e647f16892
AndersonJimenez <asjimenez@asesoftware.com>
2018-12-03 14:59:38 -0500

    Se arega solucion de estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public title = 'FrontAsesoria';
